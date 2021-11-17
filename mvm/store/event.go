dfe645bd9af9398eac0c012f9c289ff512a86325
Cedric Fung <cedric@vec.io>
2021-11-17 17:02:25 +0800

    basic pending events and processes store

--- mvm/store/event.go
+++ mvm/store/event.go
@@ -1,24 +1,101 @@
 package store
 
 import (
+	"encoding/binary"
+
 	"github.com/MixinNetwork/mixin/common"
 	"github.com/MixinNetwork/trusted-group/mvm/encoding"
+	"github.com/dgraph-io/badger/v3"
+)
+
+const (
+	prefixPendingEventQueue      = "MVM:EVENT:PENDING:QUEUE:"
+	prefixPendingEventSignatures = "MVM:EVENT:PENDING:SIGNATURES:"
 )
 
 func (bs *BadgerStore) WritePendingGroupEventAndNonce(event *encoding.Event) error {
-	panic(0)
+	return bs.Badger().Update(func(txn *badger.Txn) error {
+		proc, err := bs.readProcess(txn, event.Process)
+		if err != nil {
+			return err
+		}
+		if proc.Nonce != event.Nonce {
+			panic(event)
+		}
+		proc.Nonce = proc.Nonce + 1
+		err = bs.writeProcess(txn, proc)
+		if err != nil {
+			return err
+		}
+		key := buildPendingEventTimedKey(event)
+		val := common.MsgpackMarshalPanic(event)
+		return txn.Set(key, val)
+	})
 }
 
 func (bs *BadgerStore) ListPendingGroupEvents(limit int) ([]*encoding.Event, error) {
-	panic(0)
+	txn := bs.Badger().NewTransaction(false)
+	defer txn.Discard()
+
+	opts := badger.DefaultIteratorOptions
+	opts.PrefetchValues = false
+	opts.Prefix = []byte(prefixPendingEventQueue)
+	it := txn.NewIterator(opts)
+	defer it.Close()
+
+	var evts []*encoding.Event
+	for it.Seek(opts.Prefix); it.Valid(); it.Next() {
+		val, err := it.Item().ValueCopy(nil)
+		if err != nil {
+			return nil, err
+		}
+		var evt encoding.Event
+		err = common.MsgpackUnmarshal(val, &evt)
+		if err != nil {
+			return nil, err
+		}
+		evts = append(evts, &evt)
+		if len(evts) == limit {
+			break
+		}
+	}
+	return evts, nil
 }
 
 func (bs *BadgerStore) ReadPendingGroupEventSignatures(pid string, nonce uint64) ([][]byte, error) {
-	panic(0)
+	txn := bs.Badger().NewTransaction(false)
+	defer txn.Discard()
+
+	key := buildPendingEventSignaturesKey(pid, nonce)
+	item, err := txn.Get(key)
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	val, err := item.ValueCopy(nil)
+	if err != nil {
+		return nil, err
+	}
+	sigs := make([][]byte, len(val)/96)
+	for i := 0; i < len(sigs); i++ {
+		sigs[i] = val[i*96 : i*96+1]
+	}
+	return sigs, nil
 }
 
 func (bs *BadgerStore) WritePendingGroupEventSignatures(pid string, nonce uint64, partials [][]byte) error {
-	panic(0)
+	return bs.Badger().Update(func(txn *badger.Txn) error {
+		var val []byte
+		for _, p := range partials {
+			if len(p) != 96 {
+				panic(p)
+			}
+			val = append(val, p...)
+		}
+		key := buildPendingEventSignaturesKey(pid, nonce)
+		return txn.Set(key, val)
+	})
 }
 
 func (bs *BadgerStore) WriteSignedGroupEvent(event *encoding.Event) error {
@@ -31,4 +108,20 @@ func (bs *BadgerStore) ListSignedGroupEvents(pid string, limit int) ([]*encoding
 
 func (bs *BadgerStore) ExpireGroupEventsWithCost(events []*encoding.Event, cost common.Integer) error {
 	panic(0)
+}
+
+func buildPendingEventSignaturesKey(pid string, nonce uint64) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, nonce)
+	key := append([]byte(prefixPendingEventSignatures), pid...)
+	return append(key, buf...)
+}
+
+func buildPendingEventTimedKey(evt *encoding.Event) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, evt.Timestamp)
+	key := append([]byte(prefixPendingEventQueue), buf...)
+	key = append(key, evt.Process...)
+	binary.BigEndian.PutUint64(buf, evt.Nonce)
+	return append(key, buf...)
 }
--- mvm/store/process.go
+++ mvm/store/process.go
@@ -1,6 +1,14 @@
 package store
 
-import "github.com/MixinNetwork/trusted-group/mvm/machine"
+import (
+	"github.com/MixinNetwork/mixin/common"
+	"github.com/MixinNetwork/trusted-group/mvm/machine"
+	"github.com/dgraph-io/badger/v3"
+)
+
+const (
+	prefixProcessPayload = "MVM:PROCESS:PAYLOAD:"
+)
 
 func (bs *BadgerStore) ReadEngineGroupEventsOffset(pid string) (uint64, error) {
 	panic(0)
@@ -11,9 +19,60 @@ func (bs *BadgerStore) WriteEngineGroupEventsOffset(pid string, offset uint64) e
 }
 
 func (bs *BadgerStore) ListProcesses() ([]*machine.Process, error) {
-	panic(0)
+	txn := bs.Badger().NewTransaction(false)
+	defer txn.Discard()
+
+	opts := badger.DefaultIteratorOptions
+	opts.PrefetchValues = false
+	opts.Prefix = []byte(prefixProcessPayload)
+	it := txn.NewIterator(opts)
+	defer it.Close()
+
+	var procs []*machine.Process
+	for it.Seek(opts.Prefix); it.Valid(); it.Next() {
+		val, err := it.Item().ValueCopy(nil)
+		if err != nil {
+			return nil, err
+		}
+		var proc machine.Process
+		err = common.MsgpackUnmarshal(val, &proc)
+		if err != nil {
+			return nil, err
+		}
+		procs = append(procs, &proc)
+	}
+	return procs, nil
 }
 
 func (bs *BadgerStore) WriteProcess(p *machine.Process) error {
-	panic(0)
+	return bs.Badger().Update(func(txn *badger.Txn) error {
+		old, err := bs.readProcess(txn, p.Identifier)
+		if err != nil || old != nil {
+			return err
+		}
+		return bs.writeProcess(txn, p)
+	})
+}
+
+func (bs *BadgerStore) writeProcess(txn *badger.Txn, p *machine.Process) error {
+	key := []byte(prefixProcessPayload + p.Identifier)
+	val := common.MsgpackMarshalPanic(p)
+	return txn.Set(key, val)
+}
+
+func (bs *BadgerStore) readProcess(txn *badger.Txn, pid string) (*machine.Process, error) {
+	key := []byte(prefixProcessPayload + pid)
+	item, err := txn.Get(key)
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	val, err := item.ValueCopy(nil)
+	if err != nil {
+		return nil, err
+	}
+	var proc machine.Process
+	err = common.MsgpackUnmarshal(val, &proc)
+	return &proc, err
 }
