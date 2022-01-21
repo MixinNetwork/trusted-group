49c564a25e776d7fdcb88663d149f5cec5afbf2c
Cedric Fung <cedric@vec.io>
2022-01-21 09:20:04 +0000

    quorum registry contract send mixin transaction

--- mvm/quorum/contracts/registry.sol
+++ mvm/quorum/contracts/registry.sol
@@ -28,42 +28,42 @@ contract MixinUser is Registrable {
 }
 
 contract MixinAsset is Registrable, StandardToken {
-    uint public id;
+    uint128 public id;
 
     string public name;
     string public symbol;
     uint256 public totalSupply;
     uint8 public constant decimals = 8;
 
-    constructor(uint _id, string memory _name, string memory _symbol) {
+    constructor(uint128 _id, string memory _name, string memory _symbol) {
         registry = msg.sender;
         id = _id;
         name = _name;
         symbol = _symbol;
     }
 
-    function transfer(address _to, uint256 _value) public override returns (bool) {
-        _transfer(msg.sender, _to, _value);
-        Registry(registry).burn(_to, _value);
+    function transfer(address to, uint256 value) public override returns (bool) {
+        _transfer(msg.sender, to, value);
+        Registry(registry).burn(to, value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool) {
-        _transferFrom(_from, _to, _value);
-        Registry(registry).burn(_to, _value);
+    function transferFrom(address from, address to, uint256 value) public override returns (bool) {
+        _transferFrom(from, to, value);
+        Registry(registry).burn(to, value);
         return true;
     }
 
-    function mint(address to, uint256 amount) external onlyRegistry() {
-        balances[to] = balances[to] + amount;
-        totalSupply = totalSupply + amount;
-        emit Transfer(registry, to, amount);
+    function mint(address to, uint256 value) external onlyRegistry() {
+        balances[to] = balances[to] + value;
+        totalSupply = totalSupply + value;
+        emit Transfer(registry, to, value);
     }
 
-    function burn(address _to, uint256 _value) external onlyRegistry() {
-        balances[_to] = balances[_to] - _value;
-        totalSupply = totalSupply - _value;
-        emit Transfer(_to, registry, _value);
+    function burn(address to, uint256 value) external onlyRegistry() {
+        balances[to] = balances[to] - value;
+        totalSupply = totalSupply - value;
+        emit Transfer(to, registry, value);
     }
 }
 
@@ -77,10 +77,11 @@ contract Registry {
     event MixinTransaction(bytes);
 
     uint256[4] public GROUP;
-    uint256 public NONCE = 0;
+    uint64 public INBOUND = 0;
+    uint64 public OUTBOUND = 0;
     uint256 public constant VERSION = 1;
     mapping(address => bytes) users;
-    mapping(address => uint) assets;
+    mapping(address => uint128) assets;
 
     struct Event {
         uint64 nonce;
@@ -97,24 +98,45 @@ contract Registry {
         GROUP = _group;
     }
 
-    function claim(address asset, uint256 amount) external returns (bool) {
-        if (users[msg.sender].length == 0) {
-            return false;
-        }
-        require(assets[asset] > 0);
-        MixinAsset(msg.sender).burn(msg.sender, amount);
-        // emit MixinTransaction
+    function claim(address asset, uint256 amount) public returns (bool) {
+        require(users[msg.sender].length > 0, "invalid user");
+        require(assets[asset] > 0, "invalid asset");
+        MixinAsset(asset).burn(msg.sender, amount);
+        sendMixinTransaction(msg.sender, asset, amount);
         return true;
     }
 
     function burn(address user, uint256 amount) external returns (bool) {
+        require(assets[msg.sender] > 0, "invalid asset");
         if (users[user].length == 0) {
             return true;
         }
-        require(assets[msg.sender] > 0);
         MixinAsset(msg.sender).burn(user, amount);
-        // emit MixinTransaction
+        sendMixinTransaction(user, msg.sender, amount);
         return true;
+    }
+
+    function sendMixinTransaction(address user, address asset, uint256 amount) internal {
+        bytes memory extra = new bytes(0);
+        bytes memory log = buildMixinTransaction(OUTBOUND, users[user], assets[asset], amount, extra);
+        emit MixinTransaction(log);
+        OUTBOUND = OUTBOUND + 1;
+    }
+
+    // nonce || members || threshold || asset || amount || extra || timestamp || sig
+    function buildMixinTransaction(uint64 nonce, bytes memory receiver, uint128 asset, uint256 amount, bytes memory extra) internal view returns (bytes memory) {
+        require(extra.length < 128, "extra too large");
+        bytes memory raw = uint64ToFixedBytes(nonce);
+        raw = raw.concat(receiver);
+        raw = raw.concat(uint128ToFixedBytes(asset));
+        (bytes memory ab, uint16 al) = uint256ToVarBytes(amount);
+        raw = raw.concat(uint16ToFixedBytes(al));
+        raw = raw.concat(ab);
+        raw = raw.concat(uint16ToFixedBytes(uint16(extra.length)));
+        raw = raw.concat(extra);
+        raw = raw.concat(uint64ToFixedBytes(uint64(block.timestamp)));
+        raw = raw.concat(new bytes(2));
+        return raw;
     }
 
     function mixin(bytes memory raw) public returns (bool, bytes memory) {
@@ -124,8 +146,8 @@ contract Registry {
         uint256 offset = 0;
 
         evt.nonce = raw.toUint64(offset);
-        require(evt.nonce == NONCE, "invalid nonce");
-        NONCE = NONCE + 1;
+        require(evt.nonce == INBOUND, "invalid nonce");
+        INBOUND = INBOUND + 1;
         offset = offset + 8;
 
         (offset, evt.user) = parseEventUser(raw, offset);
@@ -173,7 +195,7 @@ contract Registry {
         size = 2 + size * 16 + 2;
         bytes memory members = raw.slice(offset, size);
         offset = offset + size;
-        return (offset, getOrCreateEventUser(members));
+        return (offset, getOrCreateUserContract(members));
     }
 
     function parseEventAsset(bytes memory raw, uint offset) public returns (uint, address) {
@@ -188,11 +210,11 @@ contract Registry {
         offset = offset + 2;
         string memory name = string(raw.slice(offset, size));
         offset = offset + size;
-        address addr = getOrCreateEventAsset(id, symbol, name);
+        address addr = getOrCreateAssetContract(id, symbol, name);
         return (offset, addr);
     }
 
-    function getOrCreateEventAsset(uint id, string memory symbol, string memory name) public returns (address) {
+    function getOrCreateAssetContract(uint128 id, string memory symbol, string memory name) public returns (address) {
         bytes memory code = getAssetContractCode(id, symbol, name);
         address asset = getContractAddress(code);
         if (assets[asset] > 0) {
@@ -205,7 +227,7 @@ contract Registry {
         return asset;
     }
 
-    function getOrCreateEventUser(bytes memory members) public returns (address) {
+    function getOrCreateUserContract(bytes memory members) public returns (address) {
         bytes memory code = getUserContractCode(members);
         address user = getContractAddress(code);
         if (users[user].length > 0) {
@@ -251,5 +273,47 @@ contract Registry {
             }
         }
         return addr;
+    }
+
+
+    function uint16ToFixedBytes(uint16 x) internal pure returns (bytes memory) {
+        bytes memory c = new bytes(2);
+        bytes2 b = bytes2(x);
+        for (uint i=0; i < 2; i++) {
+            c[i] = b[i];
+        }
+        return c;
+    }
+
+    function uint64ToFixedBytes(uint64 x) internal pure returns (bytes memory) {
+        bytes memory c = new bytes(8);
+        bytes8 b = bytes8(x);
+        for (uint i=0; i < 8; i++) {
+            c[i] = b[i];
+        }
+        return c;
+    }
+
+    function uint128ToFixedBytes(uint128 x) internal pure returns (bytes memory) {
+        bytes memory c = new bytes(16);
+        bytes16 b = bytes16(x);
+        for (uint i=0; i < 16; i++) {
+            c[i] = b[i];
+        }
+        return c;
+    }
+
+    function uint256ToVarBytes(uint256 x) internal pure returns (bytes memory, uint16) {
+        bytes memory c = new bytes(32);
+        bytes32 b = bytes32(x);
+        uint16 offset = 0;
+        for (uint16 i=0; i < 32; i++) {
+            c[i] = b[i];
+            if (c[i] > 0 && offset == 0) {
+                offset = i;
+            }
+        }
+        uint16 size = 32 - offset;
+        return (c.slice(offset, 32-offset), size);
     }
 }
